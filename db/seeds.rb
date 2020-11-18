70b7fa1f1052ea18489b31f062c1f030226186d1
Maarten Bron <maarten@roads2ranking.com>
2020-11-18 15:14:45 +0100

    set up the seeds and database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  flat = Flat.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(75..200),
+    number_of_guests: rand(1..10)
+  )
+  flat.save!
+end
