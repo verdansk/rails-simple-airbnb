b28d80604fc2d22043ae8e8b2c7cf4f338b5a0c7
Maarten Bron <maarten@roads2ranking.com>
2020-11-18 17:51:47 +0100

    finished exercise 3, rails day 3

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,48 @@
 class FlatsController < ApplicationController
 
-def index
-  @flats = Flat.all
-end
+  def index
+    # raise
+    @query = params[:query]
+    if params[:query]
+      @flats = Flat.where("name Like '%#{@query}%'")
+    else
+    @flats = Flat.all
+    end
+  end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+  params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 10.times do
   flat = Flat.new(
     name: Faker::Restaurant.name,
-    address: Faker::Address,
+    address: Faker::Address.street_address,
     description: Faker::Restaurant.description,
     price_per_night: rand(75..200),
     number_of_guests: rand(1..10)
